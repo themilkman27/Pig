d94b830ea84885df6734c104f3542d61b02ec031
Owen Johnson <owenbjohnson10@gmail.com>
2022-11-14 09:38:01 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Owen Johnson on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
